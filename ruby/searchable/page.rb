947f28726bed7ffb68fe5d11580408a0dc9a17fe
Chip Miller <charlespmiller@gmail.com>
2017-07-21 16:54:32 -0700

    Adding ruby Searchable and test examples

---
+++ ruby/searchable/page.rb
@@ -0,0 +1,39 @@
+# == Schema Information
+#
+# Table name: pages
+#
+#  id           :integer          not null, primary key
+#  parent_id    :integer
+#  slug         :string           not null
+#  title        :string           not null
+#  body         :text             not null
+#  seo_keywords :text             default("{}"), not null, is an Array
+#  hidden_link  :boolean          default("false"), not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+# Indexes
+#
+#  index_pages_on_slug  (slug) UNIQUE
+#
+
+module Page::Search
+  extend  ActiveSupport::Concern
+  include Searchable
+
+  included do
+    searchable_model # [title, preview, description, keywords, path] are already declaired
+
+    scope :search_indexable, lambda { where(hidden_link:false) }
+  end
+
+  def as_indexed_json(options={})
+    {
+      title:       title,
+      preview:     shorter_plain_text(body),
+      path:        path,
+      description: plain_text(body),
+      keywords:    seo_keywords,
+    }
+  end
+end
